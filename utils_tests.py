0458ff07ba717303171d4a3ba71c6be1759b48c8
caul-shivers <brianpham4@gmail.com>
2026-09-15 10:25:49 -0400

    Added utils_tests.py

---
+++ utils_tests.py
@@ -0,0 +1,10 @@
+import utils
+
+print(utils.Utils().reversed(1234))
+print(utils.Utils().formatter(1234))
+
+print(utils.Utils().reversed(1.234))
+print(utils.Utils().formatter(1.234))
+
+print(utils.Utils().reversed("hello"))
+print(utils.Utils().formatter("hello"))
